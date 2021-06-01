8943f05aa75a4ec7363aa9c4fb967923eb20993a
mattoriq <whitefan911@gmail.com>
2021-06-01 21:42:21 +0700

    commited

--- app/src/main/java/com/example/random/MainActivity.kt
+++ app/src/main/java/com/example/random/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.random
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn)
+        btn.setOnClickListener {
+            Toast.makeText(this@MainActivity, "BYE!", Toast.LENGTH_LONG).show()
+        }
     }
 }
